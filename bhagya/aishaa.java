9bf617051bad2d6b8a271590c5a54a90e0468f55
Aaisha <115981161+aishamohamed03@users.noreply.github.com>
2023-04-21 11:59:11 +0530

    Delete bhagya directory

--- bhagya/aishaa.java
+++
@@ -1,8 +0,0 @@
-package bhagya;
-
- class aishaa {
-      public static void main(string[] args)
-      {
-    	  System.out.print("WELCOME");
-      }
-}
--- bhagya/src/bhagya/aishaa.java
+++
@@ -1,5 +0,0 @@
-package bhagya;
-
-public class aishaa {
-
-}
